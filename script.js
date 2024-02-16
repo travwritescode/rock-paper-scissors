297eb2d633c553f1d841666d7a5219a18fc49e04
Travis Anderson <travis.anderson30@gmail.com>
2024-02-16 13:17:08 -0700

    Add playRound function for basic gameplay

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-console.log(getComputerChoice())
-
 function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3) + 1;
-
-    console.log(rand);
 
     let computerChoice;
     switch (rand) {
@@ -20,3 +16,25 @@ function getComputerChoice() {
 
     return computerChoice;
 }
+
+function playRound(userSelection, computerSelection) {
+    if (userSelection === computerSelection) {
+        return "Tie! You both chose " + userSelection + "!"
+    }
+    else if (userSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats rock."
+    }
+    else if (userSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beats paper."
+    }
+    else if (userSelection === "scissors" && computerSelection == "rock") {
+        return "You lose! Rock beats scissors."
+    }
+    else {
+        return `You win! ${userSelection} beats ${computerSelection}!`
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
