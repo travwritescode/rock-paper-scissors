27f43d3851f431dba33031b77187aded45aae597
Travis Anderson <travis.anderson30@gmail.com>
2024-02-16 13:31:12 -0700

    Implement basic game loop with scorekeeping and a nicely printed scoreboard

--- script.js
+++ script.js
@@ -22,19 +22,54 @@ function playRound(userSelection, computerSelection) {
         return "Tie! You both chose " + userSelection + "!"
     }
     else if (userSelection === "rock" && computerSelection === "paper") {
+        computerScore++;
         return "You lose! Paper beats rock."
     }
     else if (userSelection === "paper" && computerSelection === "scissors") {
+        computerScore++;
         return "You lose! Scissors beats paper."
     }
     else if (userSelection === "scissors" && computerSelection == "rock") {
+        computerScore++;
         return "You lose! Rock beats scissors."
     }
     else {
+        userScore++;
         return `You win! ${userSelection} beats ${computerSelection}!`
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function playGame() {
+    let round = 1;
+    while (round <= 5) {
+        let userChoice = prompt("Rock, paper, or scissors?").toLowerCase();
+        console.log(playRound(userChoice, getComputerChoice()));
+        round++;
+    }
+    if (userScore > computerScore) {
+        console.log(`
+            User: ${userScore}
+            Computer: ${computerScore}
+            Congratulations, you win!
+        `)
+    }
+    else if (userScore < computerScore) {
+        console.log(`
+            User: ${userScore}
+            Computer: ${computerScore}
+            You lose. Better luck next time!
+        `)
+    }
+    else {
+        console.log(`
+            User: ${userScore}
+            Computer: ${computerScore}
+            It's a draw!
+        `)
+    }
+}
+
+let userScore = 0;
+let computerScore = 0;
+
+playGame()
